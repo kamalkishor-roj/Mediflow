240e9c29ff3924b3c3f66530ad49e407ea14f36e
kamalkishor <rojkamal6@gmail.com>
2026-01-22 20:34:57 +0530

    Update streamlit_app.py

--- src/app/streamlit_app.py
+++ src/app/streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import requests
+import os
 
-API_URL = "http://127.0.0.1:8000/predict"
+API_URL = os.getenv("API_URL", "http://127.0.0.1:8000/predict")
 
 st.set_page_config(
     page_title="MediFlow – Sepsis Risk Dashboard",
